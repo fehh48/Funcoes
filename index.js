92bca0ec8c4b60b492243b8dbec1724ca5771cc5
fehh48 <felipe4813@live.com>
2024-06-13 14:40:14 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//executar a funcao, antes de executar coloquei esse cara para trabalhar
+
+torrar()   // < Esse carinha aqui
+injetarPao() // sempre "puxar a funcao, se esquecer nao roda "
+
+
+function torrar(){
+    console.log("Torrando pao")
+}
+
+//funcao dentro de funcao
+
+function injetarPao(){
+    console.log(" Preparando para colocar o pao ")
+    console.log("----------------")
+    console.log( " Aguarde o preparo ..... 3 , 2 , 1 ")
+    console.log("Seu Pao esta pronto e quentinho")
+
+}
+
+//cada funcao deve fazer uma pratica com o mesmo objetivos, se nao voce quebra 
